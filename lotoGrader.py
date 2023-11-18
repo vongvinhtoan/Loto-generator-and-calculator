e1d2f27bac98db888d20f8fed4bda23e774d2ecd
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-11-19 03:20:37 +0700

    chawcs oke

--- lotoGrader.py
+++ lotoGrader.py
@@ -9,7 +9,7 @@ class LotoGrader:
         print(f"len(self.loto_sheets) = {len(self.loto_sheets)}")
         self.loto_statistics = LotoStatistics([])
 
-    def grade(self, num_call: int, called_seq: [int]) -> None:
+    def grade(self, num_call: int, called_seq: list[int]) -> None:
         for i in range(0, num_call):
             if (i+1) % (num_call//10) == 0:
                 print(f"Calling {i + 1}/{num_call}")
@@ -23,11 +23,17 @@ class LotoGrader:
             if called_number not in called_numbers:
                 return called_number
 
-    def call(self, called_seq: [int]) -> None:
+    def call(self, called_seq: list[int]) -> None:
         for loto_sheet in self.loto_sheets:
             loto_sheet.reset()
             for loto_number in called_seq:
                 loto_sheet.mark(loto_number)
+            if loto_sheet.isComplete():
+                loto_statistic_item = LotoStatisticsItem()
+                loto_statistic_item.called_sequence = []
+                loto_statistic_item.winners.append(self.loto_sheets.index(loto_sheet))
+                self.loto_statistics.add_item(loto_statistic_item)
+                return
 
         called_numbers = called_seq.copy()
         while True:
@@ -43,13 +49,9 @@ class LotoGrader:
                 break
 
         loto_statistic_item = LotoStatisticsItem()
-        loto_statistic_item.called_sequence = called_numbers
+        loto_statistic_item.called_sequence = [x for x in called_numbers if not x in called_seq]
         for i, loto_sheet in enumerate(self.loto_sheets):
             if loto_sheet.isComplete():
                 loto_statistic_item.winners.append(i)
 
         self.loto_statistics.add_item(loto_statistic_item)
-
-            
-                
-    
